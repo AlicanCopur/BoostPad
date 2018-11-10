8d6ec1b15064e9dcc5c6710e2739bdae27629b00
Emirhan Akpınar <leetpocketmine@gmail.com>
2018-11-10 09:40:08 +0300

    Fixed and added new features

--- src/AlicanCopur/BoostPad/Main.php
+++ src/AlicanCopur/BoostPad/Main.php
@@ -3,31 +3,50 @@
 namespace AlicanCopur\BoostPad;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\Server;
-use pocketmine\Player;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\entity\Entity;
-use pocketmine\utils\Config;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\utils\Config;
 use pocketmine\math\Vector3;
-use pocketmine\level\Position;
-use pocketmine\block\Block;
 
 class Main extends PluginBase implements Listener {
-	
-	public function onEnable(){
-		@mkdir($this->getDataFolder());
-		$this->saveResource("Config.yml");
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+    protected function onEnable() {
+        @mkdir($this->getDataFolder());
+        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
+            "multiply" => 5,
+            "block-ids" => [5]
+        ]);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-    public function onMove(PlayerMoveEvent $event){
-  	  $o = $event->getPlayer();
-  	  $from = $event->getFrom();
-  	  $to = $event->getTo();
- 	   if($from->getLevel()->getBlockIdAt($from->x, $from->y - 1, $from->z) === Block::REDSTONE_BLOCK){
- 			$cfg = new Config($this->getDataFolder()."Config.yml", Config::YAML);
- 			$m = $cfg->get("multiply");
-      		$o->setMotion((new Vector3($to->x - $from->x, $to->y - $from->y, $to->z - $from->z))->multiply($m));
-  	  }
-  }
+
+    /**
+     * @param PlayerMoveEvent $event
+     */
+
+    public function move(PlayerMoveEvent $event) {
+        $player = $event->getPlayer();
+        $from = $event->getFrom();
+        $to = $event->getTo();
+        if (in_array($from->getLevel()->getBlockIdAt($from->x, $from->y - 1, $from->z), $this->getBlockIds())) {
+            $player->setMotion((new Vector3($to->x - $from->x, $to->y - $from->y, $to->z - $from->z))->multiply($this->getMultiply()));
+        }
+    }
+
+    /**
+     * @return int|null
+     */
+
+    public function getMultiply(): ?int {
+        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        return $config->get("multiply");
+    }
+
+    /**
+     * @return array|null
+     */
+
+    public function getBlockIds(): ?array {
+        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        return $config->get("block-ids");
+    }
 }
